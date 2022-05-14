a4e06413a8d9117c5d788522f7fb018b1dae4bc8
Michael Djorup <michael.djorup@gmail.com>
2022-05-13 21:40:11 -0400

    dynamic colors for the chart

--- client/src/components/MainChart/MainChart.js
+++ client/src/components/MainChart/MainChart.js
@@ -40,14 +40,15 @@ ChartJS.register(
     )
  }
 
+const testPrices = Array.from({length: 20}, () => 100 + Math.random()*10)
 
 const MainChart = ({timeframes}) => {
     
     const [currentTimeframe, setCurrentTimeframe] = useState("5d")
-
+    const [color, setColor] = useState("#00C805");
     // need one use state for each timeframe
 
-    const testPrices = Array.from({length: 20}, () => 100 + Math.random()*20)
+    
 
     const startDate = new Date(2020, 0, 1);
     const labels = [];
@@ -60,13 +61,13 @@ const MainChart = ({timeframes}) => {
         labels: labels,
         datasets: [{
             labels: "Account Balance Chart",
-            backgroundColor: "rgb(0, 200, 5)",
-            borderColor: "rgb(0, 200, 5)",
+            backgroundColor: color,
+            borderColor: color,
             borderWidth: 2,
             pointBorderWidth: 0,
             pointRadius: 0,
             pointHitRadius: 20,
-            tension: .1,
+            tension: 0.1,
             xAxisId: 'x',
             yAxisId: 'y',
             data: testPrices
@@ -94,12 +95,20 @@ const MainChart = ({timeframes}) => {
         }
     }
 
+    useEffect(() => {
+        let positive = testPrices[testPrices.length-1] >= testPrices[0]
+        if(positive){
+            setColor("#00C805")
+        } else {
+            setColor("#FF5000")
+        }
+    }, [currentTimeframe])
+
     const generateDescription = () => {
         let lastPrice = testPrices[testPrices.length-1]
         let change = testPrices[testPrices.length-1] - testPrices[0]
         let sign = change.toString().charAt(0) === '-' ? "-" : "+"
         let percentChange = 100 * (change) / testPrices[0]
-
         return sign + "$" + Math.abs(change).toFixed(2).toString() + " (" + sign + Math.abs(percentChange).toFixed(2).toString() + "%)"
     }
 
@@ -111,7 +120,7 @@ const MainChart = ({timeframes}) => {
     }
 
     return (
-        <div className='mainchart'>
+        <div className='mainchart' style={{"--color-theme": color}}>
             <p className='mainchart__balance'>{"$" + testPrices[testPrices.length-1].toFixed(2).toString()}</p>
             <p className='mainchart__description'>{generateDescription()}</p>
             <Line options={options} data={testData}/> 
