2368c3336ba954bbd0ae8b4004e22a90b9055005
Michael Djorup <michael.djorup@gmail.com>
2022-05-15 21:24:38 -0400

    comments

--- server/authentication-function/lambda_function.py
+++ server/authentication-function/lambda_function.py
@@ -1,5 +1,15 @@
 import json
 
+# https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+# https://pypi.org/project/boto3/
+# 1. reset folder - create virtual environment instead
+# 2. make sure virtual environment works - install boto3 as well
+# 3. Follow the steps to create the zip folder - also figure out the flags
+# 4. Once figured out how to deploy, test the functions using postman (might be valuable to deploy functions from command line)
+# 5. Implement the functions in this directory
+
+
+
 healthPath = "/health"
 registerPath = "/register"
 loginPath = "/login"
