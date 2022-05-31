4ecd77a12c063bf82f9b82d77791ce290a9c8adf
Michael Djorup <michael.djorup@gmail.com>
2022-05-31 17:23:30 -0400

    Added environment variables, connected to db

--- server/portfolio-app/authentication/app.py
+++ server/portfolio-app/authentication/app.py
@@ -1,6 +1,6 @@
 import json
+import os
 
-import pymongo
 # import requests
 # for testing dynamo db locally: https://medium.com/@suranjana.basu/test-your-lambda-with-aws-dynamodb-offline-locally-2efc45063fed
 
@@ -10,38 +10,10 @@ from utils.response import build_response
 healthPath = "/auth/health"
 registerPath = "/auth/register"
 loginPath = "/auth/login"
+verifyPath = "/auth/verify"
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
 
     path = event.get("path", "doesn't work")
     method = event.get("httpMethod", "doesn't work")
@@ -52,11 +24,13 @@ def lambda_handler(event, context):
         return build_response(200, "Healthy")
     elif path == registerPath and method == "POST":
         #do register logic, get register response
-        registerResponse = None
-        return registerResponse
+        return build_response(200, os.environ.get("DB_PW"))
     elif path == loginPath and method == "POST":
         loginResponse = None
         return loginResponse
+    elif path == verifyPath and method == "POST":
+        verifyResponse = None
+        return verifyResponse
     else:
         return build_response(400, event)
 
--- server/portfolio-app/authentication/utils/logic.py
+++ server/portfolio-app/authentication/utils/logic.py
@@ -1,8 +1,24 @@
 import json
+import os
+from pymongo import MongoClient
 from response import build_response
 
 
+
+db_pw = os.environ.get("DB_PW")
+jwt_secret = os.environ.get("JWT_SECRET")
+
+db_client = MongoClient(f"mongodb+srv://admin:{db_pw}@cluster0.bo9qn.mongodb.net/?retryWrites=true&w=majority")
+
+
+
+
+def generate_jwt():
+    return
+
 # checks to see if user exists in db
+# returns user object with all fields
+#if user doesn't exist, return None
 def get_user(username):
     return
 
@@ -13,10 +29,28 @@ def register(body):
     email = body.get("email")
     password = body.get("password")
 
-    # check if user exists
+    if not name or not username or not email or not password:
+        return build_response(400, "Incomplete request body")
+
+    # check if user exists by username
+
+    userinfo = get_user(username)
+
+    if userinfo:
+        return build_response(409, "Username already exists")
+    
+    # add user to db
+
+
+
+
 
 
 
 
 def login(body):
+    return
+
+
+def verify(token):
     return
